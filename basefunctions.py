e5f2033f7cd1b65c2c3da7f34b7100c39cc0d5a4
yuegelica <yuegelicay@gmail.com>
2025-07-30 11:19:43 -0500

    rename basecode.py to basefunctions.py

--- basefunctions.py
+++ basefunctions.py
@@ -3,7 +3,7 @@ import mendeleev
 import scipy.constants as const
 import re
 
-# Constants
+### Constants
 Na = const.Avogadro
 h = const.h
 hbar = const.hbar
@@ -11,9 +11,9 @@ c = const.c
 hc = h * c
 r_e = (const.e**2) / (4 * const.pi * const.epsilon_0 * const.m_e * const.c**2)
 
-# Conversion functions
+### Conversion functions
 def energy_to_wavelength(energy):
-    energy_joules = energy * const.e  #Convert eV to J
+    energy_joules = energy * const.e  # Convert eV to J
     return hc / energy_joules
 #wavelength will be in meters for these calculations
 def wavelength_to_energy(wavelength):
@@ -37,12 +37,13 @@ def wavelength_to_wavenumber(wavelength):
 def wavenumber_to_momentum(k):
     return hbar *k
 
-# Load data from Henke file
+###############################################################################
+# Load data from Henke file, skip the first row as those are the labels (str)
 def load_file(filename: str):
     energy = np.loadtxt(filename, usecols=0, skiprows=1)# in eV
     f1 = np.loadtxt(filename, usecols=1, skiprows=1)
     f2 = np.loadtxt(filename, usecols=2, skiprows=1)
-    lambda1 = energy_to_wavelength(energy)# in m
+    lambda1 = energy_to_wavelength(energy)# in meters
     return energy, f1, f2, lambda1
 
 # Attenuation coefficient mu
@@ -52,7 +53,7 @@ def attenuation(f2, rho, lambda1, element):
     mu = (rho * Na * 2 * r_e * lambda1 * f2) / Ma
     return mu
 
-#Mass attenuation coefficient mu/rho
+# Mass attenuation coefficient mu/rho
 def mass_attenuation(mu, rho):
     mass_attenuation = mu/rho
     return mass_attenuation
@@ -61,60 +62,17 @@ def mass_attenuation(mu, rho):
 def calculate_transmission(mu, x):
     return np.exp(-mu * x)
 
-#optical_depth is absorbtion length
-def optical_depth(mu, rho):
-    optical_depth = rho/mu
-    return optical_depth
-
-#Fresnel Equations to calculate how much specular reflection you expect 
-def complex_reflection(n1, n2, theta_i, theta_t):
-    theta_t = np.arcsin(n1*np.sin(theta_i/n2))
-    r_s = np.abs((n1*np.cos(theta_i)-n2*np.cos(theta_t))/(n1*np.cos(theta_i)+n2*np.cos(theta_t)))**2
-    r_p = np.abs((n2*np.cos(theta_i)-n1*np.cos(theta_t))/(n2*np.cos(theta_i)+n1*np.cos(theta_t)))**2
-    t_s = 1-r_s
-    t_p = 1-r_p
-    return r_s, r_p, t_s, t_p
-
-#unpolarized natural right
-def r_effective(r_s, r_p):
-    r_eff = 0.5*(r_s+r_p)
-    return r_eff
-
-#find delta
+# Find delta (the real refractive index) from f1
 def delta(f1, rho, lambda1):
     delta = (rho*lambda1**2)/(2*np.pi*Na*f1)
     return delta
-#find n2 from delta
+
+#Find the index of refraction n2 from delta
 def n2(delta):
     n2 = 1- delta
     return n2
 
-#special case where theta_i = theta_t = 0, so find specular reflection to show it is negligible if x-rays go thorugh a foam
-def normal_incidence(n1, n2):
-    reflectance = np.abs((n1-n2)/(n1+n2))**2
-    return reflectance
-
-# beta = imagenary refractive index
-def beta(f2, lambda1, element, rho):
-    elem= mendeleev.element(element)
-    Ma = elem.mass
-    beta = (rho*Ma*r_e*f2*lambda1**2)/(2*np.pi*Na)
-    return beta
-
-#use f1 to find othre quantities
-#actual scatter factor, plug into fresnel's 
-def critical_angle(f1, rho,lambda1, element):
-    elem = mendeleev.element(element)
-    Ma = elem.mass
-    theta_c = np.sqrt((rho*Ma*r_e*lambda1**2)/np.pi(Na))*f1
-    return theta_c
-
-
-#returns amount deflected horizontally through rectangular foam
-def refraction(theta_i,foam_length):
-    d = foam_length/ (np.tan((90-theta_i)))
-    return d 
-
+# Parses compounds (elements and counts) to use the correct files and values
 def parse_compounds(formula):
     format = r'([A-Z][a-z]*)(\d*)'
     matches = re.findall(format, formula)
@@ -128,8 +86,9 @@ def parse_compounds(formula):
             counts.append(1)
 
     return elements, counts
-    
-def weighted_ma(formula, rho, x):
+
+#for graphing transmission (transmissionplots.py)
+def weighted_ma_graph(formula, rho, x):
     elements, counts = parse_compounds(formula)
 
     mu_rho_list = []
@@ -164,23 +123,81 @@ def weighted_ma(formula, rho, x):
     mu = weighted_mu_rho * rho
     transmission = np.exp(-mu * x)
 
-    return energy_common, transmission
+    return energy_common, weighted_mu_rho
 
-#Now, consider statistical model, lots of photons come in starting parallel, given porosity of foam and nmber of pores, we calculate the distribution of horizontal refractions. 
-#Dictated by graph, and probability of location photon is hitting is uniform, the particular angle of incidence probability follows the curve
-#look in numpy random for sample
-
+# The incident angle theta_i depending on the distance from the center it hits the pore
 def angle_of_incidence(d, r):
     return np.arcsin(d/r)
 
+# Find theta_refracted
 def snells_law(theta_i, n1, n2):
-    # Calculate sin(theta_t) with clipping
     sin_theta_i = np.sin(theta_i)
     sin_theta_t = (n1/n2) * sin_theta_i
 
+    # Clip to valid range [-1, 1] to prevent arcsin errors
     sin_theta_t_clipped = np.clip(sin_theta_t, -0.999999, 0.999999)
     
-    return np.arcsin(sin_theta_t_clipped)
+    if np.abs(sin_theta_t_clipped) > 1:
+        return theta_i  # reflect, i.e., angle stays the same
+    else:
+        return np.arcsin(sin_theta_t_clipped)
 
+#Find how much a photon gets horizontally displaced ore after interacting with and being refracted through each pore
 def lateral_displacement(theta_i, theta_t, r):
     return (2*(r**2)-2*(r**2)*np.cos(np.pi-2*theta_t))**0.5*np.sin(theta_t-theta_i)
+    # return r * 2 *  ( np.tan(theta_t) - np.tan(theta_i))
+
+# Find the average pores a photon will hit from the mean free path
+def calc_avg_interactions(foam_length, pore_radius, pore_fraction):
+    mean_free_path = (4/3)*pore_radius*(1-pore_fraction)/pore_fraction
+    return int(foam_length/mean_free_path)
+
+
+###############################################################
+# Useful functions but not used directly in this sumulation #
+def critical_angle(f1, rho,lambda1, element):
+    elem = mendeleev.element(element)
+    Ma = elem.mass
+    theta_c = np.sqrt((rho*Ma*r_e*lambda1**2)/np.pi(Na))*f1
+    return theta_c
+# Optical_depth is absorption length
+def optical_depth(mu, rho):
+    optical_depth = rho/mu
+    return optical_depth
+#Porosity given density of solid and aerogel
+def calc_porosity(rho_solid, rho_eff):
+    return 1- (rho_eff/rho_solid)
+
+# Fresnel Equations to calculate how much specular reflection you expect 
+def complex_reflection(n1, n2, theta_i, theta_t):
+    theta_t = np.arcsin(n1*np.sin(theta_i/n2))
+    r_s = np.abs((n1*np.cos(theta_i)-n2*np.cos(theta_t))/(n1*np.cos(theta_i)+n2*np.cos(theta_t)))**2
+    r_p = np.abs((n2*np.cos(theta_i)-n1*np.cos(theta_t))/(n2*np.cos(theta_i)+n1*np.cos(theta_t)))**2
+    t_s = 1-r_s
+    t_p = 1-r_p
+    return r_s, r_p, t_s, t_p
+
+# Unpolarized natural right
+def r_effective(r_s, r_p):
+    r_eff = 0.5*(r_s+r_p)
+    return r_eff
+
+#Returns the distance a photons gets refracted through a rectangular slab
+def refraction(theta_i,foam_height):
+    d = foam_height/ (np.tan((90-theta_i)))
+    return d 
+
+def snells_law(theta_i, n1, n2):
+    sin_theta_i = np.sin(theta_i)
+    sin_theta_t = (n1 / n2) * sin_theta_i
+#special case where theta_i = theta_t = 0, so find specular reflection to show it is negligible if x-rays go thorugh a foam
+def normal_incidence(n1, n2):
+    reflectance = np.abs((n1-n2)/(n1+n2))**2
+    return reflectance
+
+# Find beta, (imaginary component of refractive index) from f2
+def beta(f2, lambda1, element, rho):
+    elem= mendeleev.element(element)
+    Ma = elem.mass
+    beta = (rho*Ma*r_e*f2*lambda1**2)/(2*np.pi*Na)
+    return beta
