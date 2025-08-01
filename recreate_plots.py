4589452078abd13de7fb05f9e6bc7e8f95d2038e
yuegelica <yuegelicay@gmail.com>
2025-08-01 14:59:06 -0500

    Update recreate_plots.py

--- recreate_plots.py
+++ recreate_plots.py
@@ -1,1 +1,14 @@
+# All plots were created with the following inputs
+n1=1.05
+n2 = 1.00
+N_photons = 1000
+pore_fraction = 0.985
+pore_radius = 1e-4 #1e-4 to 1e-7
+foam_height = 1700e-4 #cm
+foam_diameter = 900e-4 #cm
+energy_keV = 2.0  # keV
+formula = "Si5O10Ti"   # Compound formula
+rho = 2.33       # g/cm³
 
+#The transmissionplot.py plot was created with a path length of x
+x = 1e-5 # this is the thickness in microns of the medium
